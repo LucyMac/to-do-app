fd7797230a6cc17e93ac03243c7008540040a632
LucyMac <alessonadaykeepstheboredomaway@gmail.com>
2016-07-23 03:11:08 +0100

    first commit of vue.js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+			new Vue ({
+		    el: '#app',
+		    data: {
+		    	tasks: [
+		    		{content: 'Buy bread', completed: false, remove: false},
+		    		{content: 'Visit Kew Gardens', completed: false, remove: true},
+		    		{content: 'Call', completed: true, remove: false}
+		    	]
+
+		    }
+		});
+
+	
