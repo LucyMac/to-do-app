c96c59a24182f32eadf24a932dcbc49ca42ab211
LucyMac <alessonadaykeepstheboredomaway@gmail.com>
2016-07-25 01:49:28 +0100

    updated package.json

--- js/app.js
+++ js/app.js
@@ -1,27 +1,30 @@
-Vue.component ('todo', {  //exiting list of todos
-
-	props: ['list'], //props pass data to components
-
-	template: '#todo-template',
-
-	methods: {
-		deleteTodo: function(todo) { //method called on click of item cross icon
-			this.list.$remove(todo);
-		}
-	}
-});
-
 var vm = new Vue ({
 	el: '#app',
 	data: {
-		todos: [ 
+		newTodo: '',  //initialise newTodo
+
+		todos: localStorage.getItem('todos')   
+      	? JSON.parse(localStorage.getItem('todos'))  
+		: [  //set todos list as empty array
 		]
 	},
 	methods: {
-		addNewTodo: function() {
+		save: function() {     //persist list locally
+      		localStorage.setItem('todos', JSON.stringify(this.todos)); 
+		},
+		addNewTodo: function() {  //called both on click of + button and on keydown Enter
 			var newInput = this.newTodo; //store user input
-			this.todos.push({ content: newInput, completed: false }); //add new itemto bottom of list
+			this.todos.push({ content: newInput, completed: false }); //add new item to bottom of list
 			this.newTodo = ""; //empty input field after adding new item
+			this.save();
+		},
+		markComplete: function(todo) { //called on click of a todo item
+			todo.completed =! todo.completed;  //toggle between 2 states
+			this.save();
+		},
+		deleteTodo: function(todo) { //method called on click of item cross icon
+			this.todos.$remove(todo);
+			this.save();
 		}
  	}
 });
